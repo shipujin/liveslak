a491c083be58db611ae3c5cf6c7698e09b09eec7
Eric Hameleers <alien@slackware.com>
2020-12-30 13:45:47 +0100

    Slackware Live Edition: version 1.3.8.3
    Small version bump needed for a re-spin of the DAW ISO.
    The new kernel enabled a PC Speaker driver which grabbed the
    default hardware device and JACK does not work with it.
    Patrick has now blacklisted snd-pcsp in the eudev package,
    and all is well again.

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -35,7 +35,7 @@
 # -----------------------------------------------------------------------------
 
 # Version of the Live OS generator:
-VERSION="1.3.8.2"
+VERSION="1.3.8.3"
 
 # Timestamp:
 THEDATE=$(date +%Y%m%d)
