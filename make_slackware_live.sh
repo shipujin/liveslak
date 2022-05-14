a8b702d718f085fcf54a83dd3ba5e43c73e01ca1
Eric Hameleers <alien@slackware.com>
2022-05-14 12:41:39 +0200

    Slackware Live Edition: version 1.5.2.1

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -35,7 +35,7 @@
 # -----------------------------------------------------------------------------
 
 # Version of the Live OS generator:
-VERSION="1.5.2"
+VERSION="1.5.2.1"
 
 # Timestamp:
 THEDATE=$(date +%Y%m%d)
